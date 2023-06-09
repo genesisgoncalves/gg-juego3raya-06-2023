91e872e6d96bf474a5eab7cc7090bfe18add3854
Genesis Goncalves <g88buznego@gmail.com>
2023-06-09 18:51:06 +0200

    juego 3 en raya

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+// let turno = 0;
+// const tablero = []
+
+// const btnPulsado =(e, pos) => {
+//     turno++;
+//     const btn = e.target;
+//     const color = turno % 2 ? 'green' : 'yellow';
+//     btn
+// }
+
+
+
+// $("button").each(function(indice, elemento){
+//     elemento.addEventListener('click', (e) => btnPulsado(e, indice));
+// });
